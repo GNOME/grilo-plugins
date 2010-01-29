9201f88cdeb125863c816e87f8c9788760516eaa
Iago Toral <itoral@igalia.com>
2010-01-29 12:50:09 +0100

    Added script to automatically set MS_PLUGIN_PATH

---
+++ setup-plugins-env.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+unset MS_PLUGIN_PATH
+
+plugin_dir=src
+
+echo "Scanning directory '$plugin_dir' for plugins...."
+PLUGINS=`find $plugin_dir -type d | grep .libs`
+for p in $PLUGINS; do
+  pname=`expr $p : '.*/\(.*\)/.*'`
+  echo "  Found plugin:" $pname
+  MS_PLUGIN_PATH=$MS_PLUGIN_PATH:$PWD/$p
+done
+
+export MS_PLUGIN_PATH
+
+echo ""
+echo "Include this script as source for your current shell or just copy and paste"
+echo "the followind environment variable definition:"
+echo ""
+
+echo "------- 8< ------ 8< ------- 8< -------- 8< ------- 8< ------- 8< ------- 8< -------"
+echo export MS_PLUGIN_PATH=$MS_PLUGIN_PATH
+echo "------- 8< ------ 8< ------- 8< -------- 8< ------- 8< ------- 8< ------- 8< -------"

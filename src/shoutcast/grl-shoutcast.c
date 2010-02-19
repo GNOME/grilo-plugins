6a8a8e34350c03de020aa4da98699d6023125644
Juan A. Suarez Romero <jasuarez@igalia.com>
2010-02-19 11:46:08 +0100

    [shoutcast] Handle "count" in browsing

--- src/shoutcast/grl-shoutcast.c
+++ src/shoutcast/grl-shoutcast.c
@@ -153,6 +153,7 @@ send_genrelist_entries (OperationData *op_data)
 {
   GrlContentMedia *media;
   gchar *genre_name;
+  gint remaining;
 
   media = grl_content_box_new ();
   genre_name = (gchar *) xmlGetProp (op_data->xml_entries,
@@ -165,17 +166,25 @@ send_genrelist_entries (OperationData *op_data)
                           genre_name);
   g_free (genre_name);
 
+  if (!op_data->xml_entries->next ||
+      op_data->bs->count == 1) {
+    remaining = 0;
+  } else {
+    remaining = -1;
+  }
+
   op_data->bs->callback (op_data->bs->source,
                          op_data->bs->browse_id,
                          media,
-                         op_data->xml_entries->next? -1: 0,
+                         remaining,
                          op_data->bs->user_data,
                          NULL);
 
+  op_data->bs->count--;
   op_data->xml_entries = op_data->xml_entries->next;
   skip_garbage_nodes (&op_data->xml_entries);
 
-  if (!op_data->xml_entries) {
+  if (!op_data->xml_entries || op_data->bs->count == 0) {
     xmlFreeDoc (op_data->xml_doc);
     g_free (op_data);
     return FALSE;
@@ -193,43 +202,50 @@ send_stationlist_entries (OperationData *op_data)
   gchar *station_id;
   gchar *station_mime;
   gchar *station_name;
+  gint remaining;
 
-  if (xmlStrcmp (op_data->xml_entries->name, (const xmlChar *) "station") == 0) {
-    station_name = (gchar *) xmlGetProp (op_data->xml_entries,
-                                         (const xmlChar *) "name");
-    station_mime = (gchar *) xmlGetProp (op_data->xml_entries,
-                                         (const xmlChar *) "mt");
-    station_id = (gchar *) xmlGetProp (op_data->xml_entries,
-                                       (const xmlChar *) "id");
-    station_genre = (gchar *) xmlGetProp (op_data->xml_entries,
-                                          (const xmlChar *) "genre");
-    media_id = g_strconcat (op_data->genre, "/", station_id, NULL);
+  station_name = (gchar *) xmlGetProp (op_data->xml_entries,
+                                       (const xmlChar *) "name");
+  station_mime = (gchar *) xmlGetProp (op_data->xml_entries,
+                                       (const xmlChar *) "mt");
+  station_id = (gchar *) xmlGetProp (op_data->xml_entries,
+                                     (const xmlChar *) "id");
+  station_genre = (gchar *) xmlGetProp (op_data->xml_entries,
+                                        (const xmlChar *) "genre");
+  media_id = g_strconcat (op_data->genre, "/", station_id, NULL);
 
-    media = grl_content_audio_new ();
+  media = grl_content_audio_new ();
 
-    grl_content_media_set_id (media, media_id);
-    grl_content_media_set_title (media, station_name);
-    grl_content_media_set_mime (media, station_mime);
-    grl_content_audio_set_genre (GRL_CONTENT_AUDIO (media), station_genre);
+  grl_content_media_set_id (media, media_id);
+  grl_content_media_set_title (media, station_name);
+  grl_content_media_set_mime (media, station_mime);
+  grl_content_audio_set_genre (GRL_CONTENT_AUDIO (media), station_genre);
 
-    g_free (station_name);
-    g_free (station_mime);
-    g_free (station_id);
-    g_free (station_genre);
-    g_free (media_id);
+  g_free (station_name);
+  g_free (station_mime);
+  g_free (station_id);
+  g_free (station_genre);
+  g_free (media_id);
 
-    op_data->bs->callback (op_data->bs->source,
-                           op_data->bs->browse_id,
-                           media,
-                           op_data->xml_entries->next? -1: 0,
-                           op_data->bs->user_data,
-                           NULL);
+  if (!op_data->xml_entries->next ||
+      op_data->bs->count == 1) {
+    remaining = 0;
+  } else {
+    remaining = -1;
   }
 
+  op_data->bs->callback (op_data->bs->source,
+                         op_data->bs->browse_id,
+                         media,
+                         remaining,
+                         op_data->bs->user_data,
+                         NULL);
+
+  op_data->bs->count--;
   op_data->xml_entries = op_data->xml_entries->next;
   skip_garbage_nodes (&op_data->xml_entries);
 
-  if (!op_data->xml_entries) {
+  if (!op_data->xml_entries || op_data->bs->count == 0) {
     xmlFreeDoc (op_data->xml_doc);
     g_free (op_data);
     return FALSE;
@@ -277,7 +293,7 @@ xml_parse_result (const gchar *str, OperationData *op_data)
   }
 
   /* Check if there are elements to send*/
-  if (!op_data->xml_entries) {
+  if (!op_data->xml_entries || op_data->bs->count == 0) {
     goto finalize;
   }
 

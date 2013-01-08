0d230f2fdf99a90ca9ea3c9cf058c0a75d9ed3bf
Mathias Hasselmann <mathias@openismus.com>
2013-01-08 12:02:34 +0100

    tests: Fix static vs. non-static declaration warning

--- test/test_tmdb_full_resolution.c
+++ test/test_tmdb_full_resolution.c
@@ -27,8 +27,6 @@
 
 #define TMDB_PLUGIN_ID "grl-tmdb"
 
-const char *iso_date (const GDateTime *date, char **strbuf);
-
 /** Compare the floats.
  * A simple == will fail on values that are effectively the same,
  * due to rounding issues.
@@ -40,7 +38,8 @@ compare_floats(gfloat a, gfloat b)
   return fabs(a - b) < DBL_EPSILON;
 }
 
-static const char *iso_date (const GDateTime *date, char **strbuf)
+static const char*
+iso_date (const GDateTime *date, char **strbuf)
 {
   if (*strbuf)
     g_free (*strbuf);

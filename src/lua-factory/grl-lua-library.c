GNOME/grilo-plugins

commit e064a170316e2a737d0625720b706e4c08bae6e6

lua-factory: Also try convert HTML from ISO8859-1
The OKGoals source will try and download:
http://www.okgoals.com/page-start_from_108.0_archive_.html
which contains an ISO8859-1 accent, despite being declared as UTF-8. Try
to convert from ISO8859-1 before giving up on the download content.

https://bugzilla.gnome.org/show_bug.cgi?id=752895

src/lua-factory/grl-lua-library.c
@@ -455,9 +455,15 @@ grl_util_fetch_done (GObject *source_object,
                                   res, &data, &len, &err)) {
     data = NULL;
   } else if (!g_utf8_validate(data, len, NULL)) {
-    data = NULL;
-    g_set_error_literal (&err, G_IO_ERROR, G_IO_ERROR_INVALID_DATA,
-                         "Fetched item is not valid UTF-8");
+    char *fixed;
+    fixed = g_convert (data, len, "UTF-8", "ISO8859-1", NULL, NULL, NULL);
+    if (!fixed) {
+      g_set_error_literal (&err, G_IO_ERROR, G_IO_ERROR_INVALID_DATA,
+                           "Fetched item is not valid UTF-8 or ISO8859-1");
+      data = NULL;
+    } else {
+      data = fixed;
+    }
   }
 
   fo->results[fo->index] = (err == NULL) ? g_strdup (data) : g_strdup ("");

commit 57b345fbf40cd744f37246671df189b6d99144ce

lua-factory: Fix memory leak fetching ISO8859-1 text
Fix memory leak introduced in e064a170316e2a737d0625720b706e4c08bae6e6
when the text downloaded is actually in ISO8859-1 format.

https://bugzilla.gnome.org/show_bug.cgi?id=752895

src/lua-factory/grl-lua-library.c
@@ -450,12 +450,12 @@ grl_util_fetch_done (GObject *source_object,
   OperationSpec *os;
   FetchOperation *fo = (FetchOperation *) user_data;
   lua_State *L = fo->L;
+  gchar *fixed = NULL;
 
   if (!grl_net_wc_request_finish (GRL_NET_WC (source_object),
                                   res, &data, &len, &err)) {
     data = NULL;
   } else if (!g_utf8_validate(data, len, NULL)) {
-    char *fixed;
     fixed = g_convert (data, len, "UTF-8", "ISO8859-1", NULL, NULL, NULL);
     if (!fixed) {
       g_set_error_literal (&err, G_IO_ERROR, G_IO_ERROR_INVALID_DATA,
@@ -467,6 +467,8 @@ grl_util_fetch_done (GObject *source_object,
   }
 
   fo->results[fo->index] = (err == NULL) ? g_strdup (data) : g_strdup ("");
+  g_free (fixed);
+
   if (err != NULL) {
     GRL_WARNING ("Can't fetch element %d (URL: %s): '%s'", fo->index + 1, fo->url, err->message);
     g_error_free (err);
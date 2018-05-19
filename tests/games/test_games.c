0f6de9822191b8f6f282f48e1e75afb93884a4d6
1PunMan <saurabhsingh412@gmail.com>
2018-05-19 17:37:08 +0530

    test: Add test to check multiple genre
    Currently thegamesdb only returns genre for games with a
    single genre. This test checks that thegamesdb returns
    multiple genres for the game Shatterhand.

--- tests/games/test_games.c
+++ tests/games/test_games.c
@@ -207,6 +207,66 @@ test_resolve_thumbnails_found (void)
   g_object_unref (options);
 }
 
+static void
+test_resolve_genre_found (GrlSource *source,
+                              GList *keys,
+                              GrlOperationOptions *options,
+                              const gchar *title,
+                              const gchar *mime,
+                              guint no_of_genres)
+{
+  GError *error = NULL;
+  GrlMedia *media;
+  guint expected_n_genres;
+
+  media = build_game_media (title);
+  grl_media_set_mime (media, mime);
+
+  grl_operation_options_set_resolution_flags (options, GRL_RESOLVE_FULL);
+
+  grl_source_resolve_sync (source, media, keys, options, &error);
+
+  g_assert_no_error (error);
+
+  /* We should get a genre */
+  expected_n_genres = grl_data_length (GRL_DATA (media), GRL_METADATA_KEY_GENRE);
+  g_assert_cmpuint (expected_n_genres, ==, no_of_genres);
+
+  g_object_unref (media);
+}
+
+static void
+test_resolve_genres_found (void)
+{
+  GList *keys;
+  GrlOperationOptions *options;
+  GrlRegistry *registry;
+  GrlSource *source;
+
+  registry = grl_registry_get_default ();
+  source = grl_registry_lookup_source (registry, THEGAMESDB);
+  g_assert (source);
+
+  keys = grl_metadata_key_list_new (GRL_METADATA_KEY_GENRE,
+                                    NULL);
+
+  options = grl_operation_options_new (NULL);
+  grl_operation_options_set_resolution_flags (options, GRL_RESOLVE_FULL);
+
+  test_resolve_genre_found (source, keys, options,
+                                "Astérix",
+                                "application/x-gamegear-rom",
+                                1);
+
+  test_resolve_genre_found (source, keys, options,
+                                "Shatterhand",
+                                "application/x-nes-rom",
+                                2);
+
+  g_list_free (keys);
+  g_object_unref (options);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -222,6 +282,7 @@ main(int argc, char **argv)
 
   g_test_add_func ("/thegamesdb/resolve/good-found", test_resolve_good_found);
   g_test_add_func ("/thegamesdb/resolve/thumbnails-found", test_resolve_thumbnails_found);
+  g_test_add_func ("/thegamesdb/resolve/genre-found", test_resolve_genres_found);
 
   gint result = g_test_run ();
 

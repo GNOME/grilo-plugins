7e06bab4568b802cf29d1245e9f88148f802e906
Thiago Mendes <thiago@posteo.de>
2018-09-28 17:02:34 +0200

    lua-plugins: Code cleanup and bug fixes
     * Reduce the number of helper functions
     * Better use of the api map variable
     * Map all the supported keys
     * Check for skip equal to zero on the browser callback

--- src/lua-factory/sources/grl-radio-browser.lua
+++ src/lua-factory/sources/grl-radio-browser.lua
@@ -28,8 +28,10 @@ source = {
   id = "grl-radio-browser-lua",
   name = "Radio Browser",
   description = "A community with the aim of collecting as many internet radio stations as possible",
-  supported_keys = { "country", "codecs", "states", "languages", "tags" },
-  icon = 'resource:///org/gnome/grilo/plugins/radiofrance/radiofrance.png',
+  supported_keys = { "title", "id", "grl_radio_browser_station_counter",
+      "grl_radio_browser_country", "url", "bitrate", "genres", "thumbnail",
+      "grl_radio_browser_state", "grl_radio_browser_upvotes",
+      "grl_radio_browser_downvotes", "grl_radio_browser_clickcounter" },
   supported_media = 'audio',
   tags = { 'radio', 'net:internet', 'net:plaintext' }
 }
@@ -37,60 +39,65 @@ source = {
 -------------------
 -- API RESOURCES --
 -------------------
--- Max items
-MAX_ITEMS = 100
+-- api max itens
+MAX_ITENS = 100
 
 -- api top X
 API_TOP = 10
 
 -- do not count broken stations
-api_hidebroken = false
+api_hidebroken = true
 
 -- api urls
 api_main_url = "http://www.radio-browser.info/webservice/json"
 api_search_url = "http://www.radio-browser.info/webservice/json/stations"
 
 api_main_endpoints = {
-  {
-    id = string.format("%s/%s", api_main_url, "countries"),
-    title = "List of countries"
-  },
-  {
-    id = string.format("%s/%s", api_main_url, "codecs"),
-    title = "List of codecs"
-  },
-  {
-    id = string.format("%s/%s", api_main_url, "states"),
-    title = "List of states"
-  },
-  {
-    id = string.format("%s/%s", api_main_url, "languages"),
-    title = "List of languages"
-  },
-  {
-    id = string.format("%s/%s", api_main_url, "tags"),
-    title = "List of tags"
-  },
-  {
-    id = string.format("%s/%s", api_main_url, "stations"),
-    title = "List of all radio stations"
-  },
-  {
-    id = string.format("%s/%s/%d", api_main_url, "stations/topclick", API_TOP),
-    title = "Stations by clicks"
-  },
-  {
-    id = string.format("%s/%s/%d", api_main_url, "stations/topvote", API_TOP),
-    title = "Stations by votes"
-  },
-  {
-    id = string.format("%s/%s/%d", api_main_url, "stations/lastclick" , API_TOP),
-    title = "Stations by recent click"
-  },
-  {
-    id = string.format("%s/%s/%d", api_main_url, "stations/lastchange", API_TOP),
-    title = "Stations by recently changed/added"
-  }
+    countries = {
+      id = string.format("%s/%s", api_main_url, "countries"),
+      title = "List of countries",
+      search_key = "bycountry"
+    },
+    codecs = {
+      id = string.format("%s/%s", api_main_url, "codecs"),
+      title = "List of codecs",
+      search_key = "bycodec"
+    },
+    states = {
+      id = string.format("%s/%s", api_main_url, "states"),
+      title = "List of states",
+      search_key = "bystate"
+    },
+    languages = {
+      id = string.format("%s/%s", api_main_url, "languages"),
+      title = "List of languages",
+      search_key = "bylanguage"
+    },
+    tags = {
+      id = string.format("%s/%s", api_main_url, "tags"),
+      title = "List of tags",
+      search_key = "bytag"
+    },
+    stations = {
+      id = string.format("%s/%s", api_main_url, "stations"),
+      title = "List of all radio stations"
+    },
+    clicks = {
+      id = string.format("%s/%s/%d", api_main_url, "stations/topclick", API_TOP),
+      title = "Stations by clicks"
+    },
+    votes = {
+      id = string.format("%s/%s/%d", api_main_url, "stations/topvote", API_TOP),
+      title = "Stations by votes"
+    },
+    lastclick = {
+      id = string.format("%s/%s/%d", api_main_url, "stations/lastclick" , API_TOP),
+      title = "Stations by recent click"
+    },
+    lastchange = {
+      id = string.format("%s/%s/%d", api_main_url, "stations/lastchange", API_TOP),
+      title = "Stations by recently changed/added"
+    }
 }
 
 ------------------
@@ -98,27 +105,35 @@ api_main_endpoints = {
 ------------------
 
 function grl_source_browse(media_id)
-  if media_id == nil then
-    for _, endpoint in ipairs(api_main_endpoints) do
-      local usr_opt = create_item(endpoint)
-      grl.callback(usr_opt, -1)
+  local skip = grl.get_options("skip")
+
+  -- This is a workaround to make this plugin to work with rythmbox
+  if skip > 0 then
+      grl.callback()
+      return
+  end
+
+  if not media_id then
+    for _, endpoint in pairs(api_main_endpoints) do
+      local item = create_item(endpoint)
+      grl.callback(item, -1)
     end
     grl.callback()
     return
   end
 
   local count = grl.get_options("count")
-  local skip = grl.get_options("skip")
 
-  if skip + count > MAX_ITEMS then
-    grl.callback()
-    return
+  if count <= 0 then
+    count = MAX_ITENS
   end
 
-  local url = string.format("%s?offset=%d&limit=%d", media_id, skip, MAX_ITEMS)
+  local url = media_id
+
   if (api_hidebroken) then
     url = url.."&hidebroken=true"
   end
+
   grl.fetch(url, grl_radio_browser_now_fetch_cb, media_id)
 end
 
@@ -127,23 +142,25 @@ end
 ------------------------
 
 -- return all the media found
-function grl_radio_browser_now_fetch_cb(results, media_id)
+function grl_radio_browser_now_fetch_cb(result, media_id)
   local count = grl.get_options("count")
   local skip = grl.get_options("skip")
 
   local json = {}
-  json = grl.lua.json.string_to_table(results)
+  jsons = grl.lua.json.string_to_table(result)
 
-  for _, item in pairs(json) do
-      if skip > 0 then
-          skip = skip - 1
-      elseif count > 0 then
-          local media = create_media(item, media_id)
-          if media then
-              grl.callback(media, count)
-              count = count - 1
-          end
-      end
+  for _, json in pairs(jsons) do
+    local media = create_media(json, media_id)
+    if media then
+      grl.callback(media, count)
+      count = count - 1
+    else
+      grl.warning("Can't parse media data")
+    end
+
+    if count == 0 then
+      return
+    end
   end
   grl.callback()
 end
@@ -161,80 +178,20 @@ function create_item(item)
   return option
 end
 
--- List of countries --
-function generate_list_countries(json)
+-- Container list
+function generate_container(json, search_key)
   local media = {}
   media.type = "container"
   if json.value and string.len(json.value) > 0 then
     media.title = json.value
-    media.id = api_search_url .. "/bycountry/" .. json.value
+    media.id = string.format("%s/%s/%s", api_search_url, search_key, json.value)
   end
-  -- Plugin Specific info
+  -- Plugin Specific Keys
   if json.stationcount and string.len(json.stationcount) > 0 then
     media.grl_radio_browser_station_counter = json.stationcount
   end
-  return media
-end
-
--- List of codecs --
-function generate_list_codecs(json)
-  local media = {}
-  media.type = "container"
-  if json.value and string.len(json.value) > 0 then
-    media.title = json.value
-    media.id = api_search_url .. "/bycodec/" .. json.value
-  end
-  -- Plugin Specific info
-  if json.stationcount and string.len(json.stationcount) > 0 then
-    media.grl_radio_browser_station_counter = json.stationcount
-  end
-  return media
-end
-
--- List of states --
-function generate_list_states(json)
-  local media = {}
-  media.type = "container"
-  if json.value and string.len(json.value) > 0 then
-    media.title = json.value
-    media.id = api_search_url .. "/bystate/" .. json.value
-  end
-  -- Plugin Specific info
   if json.country and string.len(json.country) > 0 then
     media.grl_radio_browser_country = json.country
-  end
-  if json.stationcount and string.len(json.stationcount) > 0 then
-    media.grl_radio_browser_station_counter = json.stationcount
-  end
-  return media
-end
-
--- List of languages
-function generate_list_languages(json)
-  local media = {}
-  media.type = "container"
-  if json.value and string.len(json.value) > 0 then
-    media.title = json.value
-    media.id = api_search_url .. "/bylanguage/" .. json.value
-  end
-  -- Plugin Specific info
-  if json.stationcount and string.len(json.stationcount) > 0 then
-    media.grl_radio_browser_station_counter = json.stationcount
-  end
-  return media
-end
-
--- List of tags
-function generate_list_tags(json)
-  local media = {}
-  media.type = "container"
-  if json.value and string.len(json.value) > 0 then
-    media.title = json.value
-    media.id = api_search_url .. "/bytag/" .. json.value
-  end
-  -- Plugin Specific info
-  if json.stationcount and string.len(json.stationcount) > 0 then
-    media.grl_radio_browser_station_counter = json.stationcount
   end
   return media
 end
@@ -244,6 +201,7 @@ function generate_list_media(json)
   local media = {}
   media.type = "audio"
   media.mime_type = "audio/mpeg"
+  -- Grilo Metadata keys
   if json.url and string.len(json.url) > 0 then
     media.url = json.url
   else
@@ -255,9 +213,6 @@ function generate_list_media(json)
   if json.id and string.len(json.id) > 0 then
     media.id = json.stationuuid
   end
-  if json.url and string.len(json.url) > 0 then
-    media.url = json.url
-  end
   if json.bitrate and string.len(json.bitrate) > 0 then
     media.bitrate = json.bitrate
   end
@@ -267,7 +222,7 @@ function generate_list_media(json)
   if json.favicon and string.len(json.favicon) > 0 then
     media.thumbnail = json.favicon
   end
-  -- Plugin Specific info
+  -- Plugin Specific keys
   if json.codec and string.len(json.codec) > 0 then
     media.grl_radio_browser_audio_codec = json.codec
   end
@@ -293,18 +248,19 @@ function generate_list_media(json)
 end
 
 function create_media(json, id)
-  if id == api_main_endpoints[1].id then
-    return generate_list_countries(json)
-  elseif id == api_main_endpoints[2].id then
-    return generate_list_codecs(json)
-  elseif id == api_main_endpoints[3].id then
-    return generate_list_states(json)
-  elseif id == api_main_endpoints[4].id then
-    return generate_list_languages(json)
-  elseif id == api_main_endpoints[5].id then
-    return generate_list_tags(json)
+  if id == api_main_endpoints.countries.id then
+    return generate_container(json, api_main_endpoints.countries.search_key)
+  elseif id == api_main_endpoints.codecs.id then
+    return generate_container(json, api_main_endpoints.codecs.search_key)
+  elseif id == api_main_endpoints.states.id then
+    return generate_container(json, api_main_endpoints.states.search_key)
+  elseif id == api_main_endpoints.languages.id then
+    return generate_container(json, api_main_endpoints.languages.search_key)
+  elseif id == api_main_endpoints.tags.id then
+    return generate_container(json, api_main_endpoints.tags.search_key)
   else
     return generate_list_media(json)
   end
+  grl.warning("Unknown type of media")
   return nil
 end

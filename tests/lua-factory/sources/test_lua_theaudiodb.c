1d6c5908f8935ab433e13931ce81036c05b5db4d
Jean Felder <jfelder@src.gnome.org>
2019-07-27 23:18:18 +0200

    theaudiobd: Add support for artist art
    Before, we could only resolve thumbnail for albums. With this commit,
    if only artist key is set, we can resolve the artist thumbnail as
    well. That means we remove the album as a required metadata-key for
    resolve operation and allow it to be optional.
    
    The unit test is also updated to test the artist art retrieval.

--- tests/lua-factory/sources/test_lua_theaudiodb.c
+++ tests/lua-factory/sources/test_lua_theaudiodb.c
@@ -91,6 +91,72 @@ test_resolve_album_cover (void)
 }
 
 static void
+test_resolve_artist_art (void)
+{
+  guint i, j, expected_nr_thumbnails;
+  GList *keys;
+  GrlMedia *media;
+  GrlOperationOptions *options;
+  GrlSource *source;
+  GError *error = NULL;
+
+  struct {
+    gchar *artist;
+    int nr_thumbnails;
+    gchar *thumbnails[5];
+  } audios[] = {
+   { "coldplay",
+     5,
+     { "https://www.theaudiodb.com/images/media/artist/thumb/uxrqxy1347913147.jpg",
+       "https://www.theaudiodb.com/images/media/artist/clearart/ruyuwv1510827568.png",
+       "https://www.theaudiodb.com/images/media/artist/fanart/spvryu1347980801.jpg",
+       "https://www.theaudiodb.com/images/media/artist/fanart/uupyxx1342640221.jpg",
+       "https://www.theaudiodb.com/images/media/artist/fanart/qstpsp1342640238.jpg"
+     }
+   },
+   {
+     "system of a down",
+     4,
+     { "https://www.theaudiodb.com/images/media/artist/thumb/tutqyy1340536730.jpg",
+       "https://www.theaudiodb.com/images/media/artist/fanart/system-of-a-down-4def4cf79b8f7.jpg",
+       "https://www.theaudiodb.com/images/media/artist/fanart/system-of-a-down-4ddaf61d98ce2.jpg",
+       "https://www.theaudiodb.com/images/media/artist/fanart/system-of-a-down-4def4d1583fcc.jpg"
+     }
+   },
+  };
+
+  source = test_lua_factory_get_source (THEAUDIODB_ID, THEAUDIODB_OPS);
+
+  keys = grl_metadata_key_list_new (GRL_METADATA_KEY_THUMBNAIL);
+  options = grl_operation_options_new (NULL);
+  grl_operation_options_set_resolution_flags (options, GRL_RESOLVE_NORMAL);
+
+  for (i = 0; i < G_N_ELEMENTS (audios); i++) {
+    media = grl_media_audio_new ();
+    grl_media_set_artist (media, audios[i].artist);
+
+    grl_source_resolve_sync (source,
+                             GRL_MEDIA (media),
+                             keys,
+                             options,
+                             &error);
+    g_assert_no_error (error);
+
+    expected_nr_thumbnails = grl_data_length (GRL_DATA (media), GRL_METADATA_KEY_THUMBNAIL);
+    g_assert_cmpuint (expected_nr_thumbnails, ==, audios[i].nr_thumbnails);
+
+    for (j = 0; j < expected_nr_thumbnails; j++) {
+      g_assert_cmpstr (grl_media_get_thumbnail_nth (GRL_MEDIA (media), j), ==, audios[i].thumbnails[j]);
+    }
+
+    g_object_unref (media);
+  }
+
+  g_list_free (keys);
+  g_object_unref (options);
+}
+
+static void
 test_theaudiodb_setup (gint *p_argc,
                        gchar ***p_argv)
 {
@@ -116,6 +182,9 @@ main (gint argc, gchar **argv)
 
   g_test_add_func ("/lua_factory/sources/theaudiodb/resolve/albumcover",
                    test_resolve_album_cover);
+
+  g_test_add_func ("/lua_factory/sources/theaudiodb/resolve/artistart",
+                   test_resolve_artist_art);
 
   gint result = g_test_run ();
 

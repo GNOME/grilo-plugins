GNOME/grilo-plugins

commit be7223a7c504ab11cdf3215bef0964628321a74a

dleyna: Quiet error when dleyna is not installed
As the dleyna plugin's requirements are things that we will have
installed anyway (gio), we shouldn't throw warnings on startup if the
service is not available.

Lower the error to a debug so its absence is still debuggable though.

src/dleyna/grl-dleyna-servers-manager.c
@@ -135,7 +135,10 @@ grl_dleyna_servers_manager_proxy_get_servers_cb (GObject      *source_object,
 
   grl_dleyna_manager_call_get_servers_finish (priv->proxy, &object_paths, res, &error);
   if (error != NULL) {
-    GRL_WARNING ("Unable to fetch the list of available servers: %s", error->message);
+    if (!g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN))
+      GRL_DEBUG ("Unable to fetch the list of available servers: %s", error->message);
+    else
+      GRL_WARNING ("Unable to fetch the list of available servers: %s", error->message);
     g_error_free (error);
     priv->got_error = TRUE;
     return;

commit 65870231280b7f961ec649b41baff83d22b1f2a1

dleyna: Fix "Quiet error when dleyna is not installed"
Fixes: be7223a7c504 ("dleyna: Quiet error when dleyna is not installed")

src/dleyna/grl-dleyna-servers-manager.c
@@ -135,7 +135,7 @@ grl_dleyna_servers_manager_proxy_get_servers_cb (GObject      *source_object,
 
   grl_dleyna_manager_call_get_servers_finish (priv->proxy, &object_paths, res, &error);
   if (error != NULL) {
-    if (!g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN))
+    if (g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN))
       GRL_DEBUG ("Unable to fetch the list of available servers: %s", error->message);
     else
       GRL_WARNING ("Unable to fetch the list of available servers: %s", error->message);